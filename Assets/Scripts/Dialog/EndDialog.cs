6cb2d3da9e4304faf9389e060d1ac1fa4e5a3ea8
fxd <mygoddess007700@gmail.com>
2022-01-28 11:41:53 +0800

    :sparkles: feat: 控制cg切换

---
+++ Assets/Scripts/Dialog/EndDialog.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndDialog : MonoBehaviour
+{
+    public Sprite sprite;
+    public Image image;
+
+    private float prepareTime;
+
+    private bool loadImg = true;
+    private bool loadScene = false;
+
+    void Start()
+    {
+        prepareTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown && loadScene)
+        {
+            Fade.instance.LoadNextScene();
+            loadScene = false;
+        }
+        if (Input.anyKeyDown && Time.time > prepareTime + 2f && loadImg)
+        {
+            Fade.instance.LoadNextImg(sprite, image);
+            Invoke("LoadSceneToTrue", 2f);
+            loadImg = false;
+        }   
+    }
+
+    void LoadSceneToTrue()
+    {
+        loadScene = true;
+    }
+}
