1f0351f622c7304e72d713dbfcaa63b7c2ccc795
fxd <mygoddess007700@gmail.com>
2022-01-28 11:54:21 +0800

    :sparkles: feat: 目前仅控制游戏失败

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    void Start()
+    {
+        instance = this;    
+    }
+
+    public void GameDefeat()
+    {
+        Fade.instance.LoadEndScene();
+    }
+}
